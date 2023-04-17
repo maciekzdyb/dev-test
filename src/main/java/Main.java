2f44eb8ab62b5edb4e77822537191d12ec3901f1
Maciek <maciekzdyb@gmail.com>
2023-04-17 15:45:08 +0200

    add loop to choose tasks in main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
-        Task1 task1 = new Task1();
-        task1.calculate();
+        System.out.println("Choose task: enter 1, 2 or 3");
+        System.out.println("Exit, enter q");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        while (!(input = scanner.nextLine()).equals("q")){
+            switch (input) {
+                case "1" -> {
+                    System.out.println("Taks 1, enter input:");
+                    Task1 task1 = new Task1();
+                    task1.calculate();
+                }
+                case "2" -> {
+                    System.out.println("Taks 2, enter input:");
+                    Task2 task2 = new Task2();
+                    task2.calculate();
+                }
+                case "3" -> {
+                    System.out.println("Taks 3, enter input:");
+                    Task3 task3 = new Task3();
+                    task3.calculate();
+                }
+            }
+        }
 
-//        Task2 task2 = new Task2();
-//        task2.calculate();
-
-//        Task3 task3 = new Task3();
-//        task3.calculate();
     }
 }
